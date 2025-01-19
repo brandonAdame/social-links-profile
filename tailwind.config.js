aa04f50ce033989cd8aeb9288d7e46df61cbdbf0
Brandon Adame <badamegachuz@gmail.com>
2025-01-18 22:00:03 -0500

    enhancements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		colors: {
-  			'vite-bg': '#242424',
-  			'origin': '#c5f82a'
-  		},
-  		fontFamily: {
-  			inter: [
-  				'Inter',
-  				'sans-serif'
-  			]
-  		},
-  	}
+    extend: {
+      colors: {
+        "vite-bg": "#242424",
+        origin: "#c5f82a",
+        foreground: "hsl(var(--foreground))",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      keyframes: {
+        "flip-words": {
+          "10%": { transform: "translateY(-112%)" },
+          "25%": { transform: "translateY(-100%)" },
+          "35%": { transform: "translateY(-212%)" },
+          "50%": { transform: "translateY(-200%)" },
+          "60%": { transform: "translateY(-312%)" },
+          "75%": { transform: "translateY(-300%)" },
+          "85%": { transform: "translateY(-412%)" },
+          "100%": { transform: "translateY(-400%)" },
+        },
+      },
+      animation: {
+        "flip-words": "flip-words 8s infinite",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
